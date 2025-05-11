c491be5aefc5a9de516699b3ac0452a5d88e039a
GeekUNinine <geekuninine@gmail.com>
2025-05-11 14:34:57 -0300

    commit

---
+++ backend/src/main/java/com/barbearia/backend/config/CorsGlobalConfig.java
@@ -0,0 +1,24 @@
+package com.barbearia.backend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsGlobalConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://projeto-barbearia-7iyi.vercel.app")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
